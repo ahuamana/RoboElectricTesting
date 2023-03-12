05ffde691cb6e2576e56429b2accf27c32e99ed6
Antony Nicolas Huaman Alikhan <73962071@continental.edu.pe>
2023-03-12 16:36:47 -0500

    testing with expresso with dinamics texts

--- app/src/androidTest/java/com/cursosandroidant/inventory/mainModule/view/MainActivityTest2.kt
+++ app/src/androidTest/java/com/cursosandroidant/inventory/mainModule/view/MainActivityTest2.kt
@@ -4,7 +4,9 @@ package com.cursosandroidant.inventory.mainModule.view
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.*
 import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
@@ -40,7 +42,27 @@ class MainActivityTest2 {
         onView(withId(R.id.action_history))
             .perform(click())
 
+        var snackbar_msg = ""
+        activityScenarioRule.scenario.onActivity {
+            snackbar_msg = it.getString(R.string.main_msg_go_history)
+        }
+
         onView(withId(com.google.android.material.R.id.snackbar_text))
-            .check(matches(withText("Ir al historial…")))
+            .check(matches(withText(snackbar_msg)))
+    }
+
+    @Test
+    fun contextMenu_menuItemClick_returnMsg(){
+        //Recomendado para menús de contexto
+        onView(withId(R.id.recyclerView))
+            .perform(click())
+
+        openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext())
+
+        onView(withText("Salir"))
+            .perform(click())
+
+        onView(withId(com.google.android.material.R.id.snackbar_text))
+            .check(matches(withText("Salir…")))
     }
 }
